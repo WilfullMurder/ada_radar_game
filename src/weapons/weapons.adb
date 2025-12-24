83d7e9af493c797472943c755ab090683480c98e
WilfullMurder <mrfarrow2013@gmail.com>
2025-12-24 11:20:03 +0000

    - Adds base weapon class and interface. -Implements base weapon type with default cooldown and owner plumbing

---
+++ src/weapons/weapons.adb
@@ -0,0 +1,48 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with GL.Types; use GL.Types;
+
+package body Weapons is
+   overriding procedure Initialize(Self : in out Weapon_Base; Owner : not null access Entity.Entity_Interface'Class) is
+   begin
+      Self.Owner := Owner;
+   end Initialize;
+
+
+   overriding procedure Update(Self : in out Weapon_Base; Delta_Time : GL.Types.Double) is
+   begin
+      if Self.Auto_Fire and then Self.Can_Fire(Delta_Time) and then Delta_Time >= GL.Types.Double(Self.Next_Fire_Time) then
+         Self.Fire(Delta_Time);
+         Self.Next_Fire_Time := Delta_Time + GL.Types.Double(Self.Cooldown);
+      end if;
+   end Update;
+
+   overriding procedure On_Input_Fire(Self : in out Weapon_Base; Delta_Time : GL.Types.Double) is
+   begin
+      -- default: no-op for non-input weapons
+      null;
+   end On_Input_Fire;
+
+
+   overriding procedure Set_Target(Self : in out Weapon_Base; Target : Entity.Entity_Ref) is
+   begin
+      Self.Target := Target;
+   end Set_Target;
+
+   overriding function Can_Fire(Self : Weapon_Base; Delta_Time : GL.Types.Double) return Boolean is
+   begin
+      return Delta_Time >= Self.Next_Fire_Time;
+   end Can_Fire;
+
+   overriding procedure Fire(Self : in out Weapon_Base; Delta_Time : GL.Types.Double) is
+   begin
+      Put_Line("Weapon fired (base)");
+   end Fire;
+
+
+
+   overriding procedure Cleanup(Self : in out Weapon_Base) is
+   begin
+      -- default: no-op
+      null;
+   end Cleanup;
+end Weapons;
